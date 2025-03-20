0d46b244057fb1c76c928458ea2d7362551a5a55
Gerry <zgbartol@hotmail.com>
2025-03-20 15:27:29 -0300

    Marzo del 2025

---
+++ python3/Emoticones.py
@@ -0,0 +1,19 @@
+emoticones = [
+    "\U0001F600",  # 😀
+    "\U0001F602",  # 😂
+    "\U0001F60D",  # 😍
+    "\U0001F914",  # 🤔
+    "\U0001F62D",  # 😭
+    "\U0001F60E",  # 😎
+    "\U0001F44D",  # 👍
+    "\U0001F44E",  # 👎
+    "\U0001F389",  # 🎉
+    "\U0001F4BB",  # 💻
+    "\U0001F499",  # 💙 (corazón)
+    "\U0001F498",  # no se como escribir un emoticon con el teclado
+
+    "\U0001F525"   # 🔥 (fuego)
+]
+
+for emo in emoticones:
+    print(emo)
